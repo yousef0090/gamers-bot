3309438d38623ab92b6679e474a8d26b0a1a51a9
yousef0090 <54067011+yousef0090@users.noreply.github.com>
2019-08-13 14:28:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,1 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-console.log("Scrpit By Dream");
 
-
-client.on("ready", () => {
-let channel =     client.channels.get("اي دي الروم")
-setInterval(function() {
-channel.send(`اكتب هنا ااش تريد يكتب فب السبام`);
-}, 30)
-})
-
-client.login(process.env.BOT_TOKEN);
