63950d0bef1ff7058f9fb894e08481122ed1e0f6
yousef0090 <54067011+yousef0090@users.noreply.github.com>
2019-08-13 14:57:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,11 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By NaSto");
 
+
+
+
+
+
+
+client.login("NjEwNjA0MjkzNjM2MjI3MTAy.XVHseQ.bM4uenlzsfVm2DMtwpzl4lhUXRM");
