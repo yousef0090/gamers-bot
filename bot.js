f22510346a66ec6bcd13f3d04757a00175e793a9
yousef0090 <54067011+yousef0090@users.noreply.github.com>
2019-08-13 21:15:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,1 @@
-client.on("message", msg=>{
 
-if(!msg.content.startsWith(${prefix}tax)) return;
-
-let tax = msg.content.split(" ")[1]
-
-let Price = msg.content.split(" ")[2];
-
-if(!tax  !Price) return msg.reply(\${prefix}tax 15% 100000``).then(z=>z.delete(3000));
-
-tax = tax.replace(/%/g,"");
-
-let resulting = Math.floor(Price-(Price*(tax/100)));
-
-if(!resulting  resulting < 0 ||  isNaN(resulting)) return msg.reply(\${prefix}tax 15% 100000`).then(z=>z.delete(3000));
-
-msg.reply(resulting is ${resulting}$`)
-
-})
