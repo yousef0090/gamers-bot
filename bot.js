bceaf27c6068f2a9f0517b45eecaf19dadeec5b5
yousef0090 <54067011+yousef0090@users.noreply.github.com>
2019-08-13 15:22:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,3 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-console.log("Scrpit By Dream");
-
 
 client.on("ready", () => {
 let channel =     client.channels.get("564850883616309278")
