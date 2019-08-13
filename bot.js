49c247c1625b1ce1bd3a2af2687761fdddc50047
yousef0090 <54067011+yousef0090@users.noreply.github.com>
2019-08-13 15:17:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("اي دي الروم")
+let channel =     client.channels.get("564850883616309278")
 setInterval(function() {
 channel.send(`اكتب هنا ااش تريد يكتب فب السبام`);
 }, 30)
